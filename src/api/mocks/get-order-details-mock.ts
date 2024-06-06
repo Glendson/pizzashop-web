37cf5f89f37f2d07dc01d49b7d61f1dfc68a5193
Glendson <gztomazetto@gmail.com>
2024-06-06 17:41:36 +0100

    feat: added mocks to tests E2E

---
+++ src/api/mocks/get-order-details-mock.ts
@@ -0,0 +1,32 @@
+import { http, HttpResponse } from 'msw'
+
+import {
+  GetOrderDetailsParams,
+  GetOrderDetailsResponse,
+} from '../get-order-details'
+
+export const getOrderDetailsMock = http.get<
+  GetOrderDetailsParams,
+  never,
+  GetOrderDetailsResponse
+>('/orders/:orderId', ({ params }) => {
+  return HttpResponse.json({
+    id: params.orderId,
+    customer: {
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      phone: '13253251',
+    },
+    status: 'pending',
+    createdAt: new Date().toISOString(),
+    totalInCents: 5000,
+    orderItems: [
+      {
+        id: 'order-item-1',
+        priceInCents: 5000,
+        product: { name: 'calabresa' },
+        quantity: 1,
+      },
+    ],
+  })
+})
--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -2,24 +2,42 @@ import { setupWorker } from 'msw/browser'
 
 import { env } from '@/env'
 
+import { approveOrderMock } from './approve-order-mock'
+import { cancelOrderMock } from './cancel-order-mock'
+import { deliverOrderMock } from './deliver-order-mock'
+import { dispatchOrderMock } from './dispatch-order-mock'
 import { getDailyRevenueInPeriodMock } from './get-daily-revenue-in-period-mock'
 import { getDayOrdersAmountMock } from './get-day-orders-amout-mock'
+import { getManagedRestaurantMock } from './get-managed-restaurant-mock'
 import { getMonthCanceledOrdersAmountMock } from './get-month-canceled-orders-amount-mock'
 import { getMonthOrdersAmountMock } from './get-month-orders-amount-mock'
 import { getMonthRevenueMock } from './get-month-revenue-mock'
+import { getOrderDetailsMock } from './get-order-details-mock'
+import { getOrdersMock } from './get-orders-mock'
 import { getPopularProductsMock } from './get-popular-products-mock'
+import { getProfileMock } from './get-profile-mock'
 import { registerRestaurantMock } from './register-restaurant-mock'
 import { signInMock } from './sign-in-mock'
+import { updateProfileMock } from './update-profile-mock'
 
 export const worker = setupWorker(
   getMonthCanceledOrdersAmountMock,
+  getDailyRevenueInPeriodMock,
   getMonthOrdersAmountMock,
+  getManagedRestaurantMock,
   registerRestaurantMock,
   getDayOrdersAmountMock,
-  getMonthRevenueMock,
-  signInMock,
   getPopularProductsMock,
-  getDailyRevenueInPeriodMock,
+  getMonthRevenueMock,
+  getOrderDetailsMock,
+  updateProfileMock,
+  dispatchOrderMock,
+  approveOrderMock,
+  deliverOrderMock,
+  cancelOrderMock,
+  getProfileMock,
+  getOrdersMock,
+  signInMock,
 )
 
 export async function enableMSW() {
